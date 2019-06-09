9a906d6ca8c9148535a3d896dd5d45cc640a1835
Keshav Tangri <tangri57@gmail.com>
2019-06-09 21:29:13 +0530

    Escape Characters

--- exercise10/ex10.py
+++ exercise10/ex10.py
@@ -15,8 +15,28 @@ print backslash_cat
 print fat_cat
  
 
-while True:
-	for i in ["/","-","|","\\","-","|"]:
-		print "%s\r"%i,
+#while True:
+#	for i in ["/","-","|","\\","-","|"]:
+#		print "%s\r"%i,
+
+
+sample = 'This is a sample\'s String \n It is used to tell you difference between %s and %r '
+
+printStyle = "When we use %%s we get : %s and with %%r we get : %r"
+print printStyle % (sample,sample)
+
+sampleBack = "Here we will use Back\bSpace Escape Sequence...\nYou must have obsereved that the 'k' in back is omitted by the \\b escape sequence.!\nLet\'s try and have formfeed\fand here we are just below d :P"
+print sampleBack
+
+compare = "Let\'s compare the \\\' and \\\" usage in this \"sentence\" !"
+
+print printStyle % (compare,compare)
+
+print '''
+	In this, we're printing 
+	using single triple quotes 
+	instead of triple double quotes """
+	to see different style of using triple quotes
+	'''
 
 
