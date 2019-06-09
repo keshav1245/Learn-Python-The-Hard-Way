f4d73e03808c9975c37e86886d0253a1983d05eb
Keshav Tangri <tangri57@gmail.com>
2019-06-09 13:27:34 +0530

    Commnets n Pound

---
+++ exercise2/ex2.py
@@ -0,0 +1,9 @@
+# This is a comment !
+# Anything after # is ignored by python
+
+print "I could have code like this !" #and the comment after this is ignored !
+
+# We can use comment to 'disable' or comment out a piece of code:
+# print "Rhis won't run"
+
+print "This will run !"
