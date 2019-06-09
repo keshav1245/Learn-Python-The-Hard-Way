94a73628dd2730e77132cad773ccc285ff09e2cc
Keshav Tangri <tangri57@gmail.com>
2019-06-09 21:32:34 +0530

    The first Program

---
+++ exercise01/ex1.py
@@ -0,0 +1,12 @@
+
+#The first program
+
+print "Hello World"
+print "Hello Again"
+print "I like typing this !"
+print "This is fun."
+print "Yay ! Printing"
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print "This line will ne printed as it is not commented"
+#print "This line will not be printed as it is commented"
