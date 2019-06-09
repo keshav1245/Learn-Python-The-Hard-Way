fa9440d94f5420d510460bb759a0ee89f4f5fb55
Keshav Tangri <tangri57@gmail.com>
2019-06-09 16:09:13 +0530

    More Printing

---
+++ exercise7/ex7.py
@@ -0,0 +1,25 @@
+#It will print this string text
+print "Mary had a little lamb."
+#It will print the below string text and also add snow in place of %s
+print "Its fleece was white as %s"%"snow"
+#Simple printing string
+print "And everywhere that Mary went."
+#This print . 10 times in a line ..........
+print "."*10 
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+print end1 + end2 + end3 + end4 + end5 + end6, 
+print end7 + end8 + end9 + end10 + end11 + end12
+
