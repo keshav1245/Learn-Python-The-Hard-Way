2c14b88b0d8c424a7012538cb11b58ed9ea139ee
Keshav Tangri <tangri57@gmail.com>
2019-06-09 13:30:56 +0530

    Numbers and Maths

---
+++ exercise3/ex3.py
@@ -0,0 +1,28 @@
+#This will just print chars on screen
+print "I will now count my chickens:"
+#print Hens concatenated by no of hens according to the Expression
+print "Hens ",25+30/6
+#print Roosters concatenated by no of hens according to the Expression
+print "Roosters ",100 - 25 *3 % 4
+#This will just print chars on screen
+print "Now i will count the eggs : "
+##print int by cacl below
+print 3+2+1-5+4%2 -1/4 +6
+# print just a line
+print "Is it true that 3 + 2 < 5 - 7 ? "
+# prints true or false 
+print 3+2 <5-7
+#addition of LHS
+print "What is 3 + 2 ? ",3+2
+#Subtraction og RHS
+print "What is 5-7 ? ",5-7
+#just prints a line
+print "Oh, that's why it is false."
+#just prints a line
+print "How about some more..."
+#Prints true or false depending on if 5 greater than -2
+print "Is it greater? ",5 > -2
+#Prints true or false depending on if 5 greater than or equal to -2
+print "Is it greater or equal ? ",5>=-2
+#Prints true or false depending on if 5 less than or equal to -2
+print "Is it less or equal ? ",5<=-2
