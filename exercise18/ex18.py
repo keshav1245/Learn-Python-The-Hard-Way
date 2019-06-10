ff688852507aa63dcf889a9a059ed4dcb3d2d043
Keshav Tangri <tangri57@gmail.com>
2019-06-10 16:36:11 +0530

    Names, Variables, Code, Functions

---
+++ exercise18/ex18.py
@@ -0,0 +1,21 @@
+def print_two(*args):
+	arg1,arg2 = args
+	print "arg1: %r, arg2: %r"%(arg1,arg2)
+
+def print_two_again(arg1,arg2):
+	print "arg1: %r, arg2: %r"%(arg1,arg2)
+
+def print_one(arg1):
+	print "arg1: %r"%arg1
+
+def print_none():
+	print "I got nothin !"
+
+print_two("Keshav",20)
+
+print_two_again("Zukayu","Keshav")
+
+print_one("Tangri")
+
+print_none()
+
