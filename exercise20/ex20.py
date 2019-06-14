56fc6b86206b8e4f78b9e5b835280f500266f007
Keshav Tangri <tangri57@gmail.com>
2019-06-14 21:45:35 +0530

    Functions and files

---
+++ exercise20/ex20.py
@@ -0,0 +1,31 @@
+from sys import argv
+
+script, input_file = argv
+
+def print_all(f):
+	print f.read()
+
+def rewind(f):
+	f.seek(0)
+
+def print_a_line(line_count,f):
+	print line_count,f.readline()
+
+current_file = open(input_file)
+
+print "First let's print the whole file:\n"
+
+print_all(current_file)
+
+print "Now let's reqind, kind of like a tape."
+
+rewind(current_file)
+
+print "Let's print three lines:"
+
+current = 1
+print_a_line(current,current_file)
+current = current + 1
+print_a_line(current,current_file)
+current = current +1
+print_a_line(current,current_file)
